fbc0dc3775e52302526045cc5073274dd398edc6
Harshul Sharma <harshulsharma000@outlook.com>
2017-07-29 15:46:57 +0530

    Eslint, React Navigation and other facilities integrated.

---
+++ src/config/routes.js
@@ -0,0 +1,11 @@
+import Welcome from '../screens/welcome';
+import Authen from '../screens/authen';
+import Mainscreen from '../screens/main';
+
+const Routes = {
+    Welcome: { screen: Welcome },
+    Auth: { screen: Authen },
+    Main: { screen: Mainscreen }
+    };
+
+export default Routes;
---
+++ src/screens/authen.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+class Welcome extends Component {
+    render() {
+        return (
+            <View style={styles.container} >
+                <Text> HI there you are in authen </Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default Welcome;
